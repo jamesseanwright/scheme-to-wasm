bb0a93e5e862b7b0460d741447089bd3b20e4fed
James Wright <james@jamesswright.co.uk>
2019-12-16 20:33:26 +0000

    Adds header data

--- src/compiler/index.ts
+++ src/compiler/index.ts
@@ -1,3 +1,14 @@
-const compile = (source: string) => [];
+const bytePad = (bytes: number[], totalByteLength: number) => [
+  ...bytes,
+  ...Array(totalByteLength - bytes.length).fill(0x0),
+];
+
+const magicNumber = [0x0, 0x61, 0x73, 0x6d];
+const wasmVersion = bytePad([0x1], 4);
+
+const compile = (source: string) => [
+  ...magicNumber,
+  ...wasmVersion,
+];
 
 export default compile;

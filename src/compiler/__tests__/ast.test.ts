02863631818eeadbf7473236c8884a755f795ac7
James Wright <james@jamesswright.co.uk>
2019-12-22 22:25:51 +0000

    Adds test case for multi-sexp function bodies and non-func defs

--- src/compiler/__tests__/ast.test.ts
+++ src/compiler/__tests__/ast.test.ts
@@ -113,5 +113,147 @@ describe('abstract syntax tree', () => {
     });
   });
 
-  it.todo('should support multiple expressions in lambdas, treating the last as the return');
+  it('should support multiple expressions in lambdas', () => {
+    // (define operate
+    //   (lambda (x y)
+    //     (define mutiplied (* x y))
+    //     (define divided (/ x y))
+    //     (+ mutiplied divided)
+    //   )
+    // )
+
+    // (operate 2 3)
+
+    const tokens: Token[] = [
+      { type: 'paren', value: '(' },
+      { type: 'keyword', value: 'define' },
+      { type: 'identifier', value: 'operate' },
+      { type: 'paren', value: '(' },
+      { type: 'keyword', value: 'lambda' },
+      { type: 'paren', value: '(' },
+      { type: 'identifier', value: 'x' },
+      { type: 'identifier', value: 'y' },
+      { type: 'paren', value: ')' },
+      { type: 'paren', value: '(' },
+      { type: 'keyword', value: 'define' },
+      { type: 'identifier', value: 'mutiplied' },
+      { type: 'paren', value: '(' },
+      { type: 'operator', value: '*' },
+      { type: 'identifier', value: 'x' },
+      { type: 'identifier', value: 'y' },
+      { type: 'paren', value: ')' },
+      { type: 'paren', value: ')' },
+      { type: 'paren', value: '(' },
+      { type: 'keyword', value: 'define' },
+      { type: 'identifier', value: 'divided' },
+      { type: 'paren', value: '(' },
+      { type: 'operator', value: '/' },
+      { type: 'identifier', value: 'x' },
+      { type: 'identifier', value: 'y' },
+      { type: 'paren', value: ')' },
+      { type: 'paren', value: ')' },
+      { type: 'paren', value: '(' },
+      { type: 'operator', value: '+' },
+      { type: 'identifier', value: 'mutiplied' },
+      { type: 'identifier', value: 'divided' },
+      { type: 'paren', value: ')' },
+      { type: 'paren', value: ')' },
+      { type: 'paren', value: ')' },
+      { type: 'paren', value: '(' },
+      { type: 'identifier', value: 'operate' },
+      { type: 'number', value: '2' },
+      { type: 'number', value: '3' },
+      { type: 'paren', value: ')' },
+    ];
+
+    expect(buildAST(tokens)).toEqual({
+      type: 'program',
+      body: [
+        {
+          type: 'definition',
+          identifier: {
+            type: 'identifier',
+            name: 'operate'
+          },
+          value: {
+            type: 'function',
+            params: [
+              {
+                type: 'identifier',
+                name: 'x'
+              },
+              {
+                type: 'identifier',
+                name: 'y'
+              }
+            ],
+            body: [
+              {
+                type: 'definition',
+                identifier: {
+                  type: 'identifier',
+                  name: 'mutiplied'
+                },
+                value: {
+                  type: 'binaryExpression',
+                  operator: '*',
+                  left: {
+                    type: 'identifier',
+                    name: 'x'
+                  },
+                  right: {
+                    type: 'identifier',
+                    name: 'y'
+                  }
+                }
+              },
+              {
+                type: 'definition',
+                identifier: {
+                  type: 'identifier',
+                  name: 'divided'
+                },
+                value: {
+                  type: 'binaryExpression',
+                  operator: '/',
+                  left: {
+                    type: 'identifier',
+                    name: 'x'
+                  },
+                  right: {
+                    type: 'identifier',
+                    name: 'y'
+                  }
+                }
+              },
+              {
+                type: 'binaryExpression',
+                operator: '+',
+                left: {
+                  type: 'identifier',
+                  name: 'multiplied'
+                },
+                right: {
+                  type: 'identifier',
+                  name: 'divided'
+                }
+              },
+              {
+                type: 'identifier',
+                name: 'operate'
+              },
+              {
+                type: 'literal',
+                value: '2'
+              },
+              {
+                type: 'literal',
+                value: '3'
+              }
+            ]
+          }
+        }
+      ]
+    });
+  });
 });

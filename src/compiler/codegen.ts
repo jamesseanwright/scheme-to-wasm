efddf5f59eb24f3bf1f0230de6a7a2d1b383ffd1
James Wright <james@jamesswright.co.uk>
2019-12-23 18:27:48 +0000

    Moves WASM header into codegen

--- src/compiler/codegen.ts
+++ src/compiler/codegen.ts
@@ -1,5 +1,16 @@
 import { Program } from './ast';
 
-const generateBytecode = (program: Program) => [''];
+const bytePad = (bytes: number[], totalByteLength: number) => [
+  ...bytes,
+  ...Array(totalByteLength - bytes.length).fill(0x0),
+];
+
+const magicNumber = [0x0, 0x61, 0x73, 0x6d];
+const wasmVersion = bytePad([0x1], 4);
+
+const generateBytecode = (program: Program) => [
+  ...magicNumber,
+  ...wasmVersion,
+];
 
 export default generateBytecode;
--- src/compiler/index.ts
+++ src/compiler/index.ts
@@ -1,11 +1,3 @@
-const bytePad = (bytes: number[], totalByteLength: number) => [
-  ...bytes,
-  ...Array(totalByteLength - bytes.length).fill(0x0),
-];
-
-const magicNumber = [0x0, 0x61, 0x73, 0x6d];
-const wasmVersion = bytePad([0x1], 4);
-
-const compile = (source: string) => [...magicNumber, ...wasmVersion];
+const compile = (source: string) => [];
 
 export default compile;

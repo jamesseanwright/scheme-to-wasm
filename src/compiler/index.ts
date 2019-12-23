440047cee498588e69281e3dc3c462204e489982
James Wright <james@jamesswright.co.uk>
2019-12-23 18:46:19 +0000

    Calls units within compiler entry point

--- src/compiler/index.ts
+++ src/compiler/index.ts
@@ -1,3 +1,12 @@
-const compile = (source: string) => [];
+import tokenise from './tokeniser';
+import buildAST from './ast';
+import generateBytecode from './codegen';
+
+const compile = (source: string) => {
+  const tokens = tokenise(source);
+  const ast = buildAST(tokens);
+
+  return generateBytecode(ast);
+};
 
 export default compile;
